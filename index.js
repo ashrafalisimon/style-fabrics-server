00ea5f5c0379b17b33865f09173104b45f066119
Ashraf Ali <ashrafalicitp@gmail.com>
2022-05-24 03:10:45 +0600

    product api load

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const app = express()
-require('dotenv').config()
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT ||5000
 
 app.use(cors())
 app.use(express.json())
 
 
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rt9od.mongodb.net/?retryWrites=true&w=majority`;
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.asxnk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try{
+        await client.connect();
+        const productCollection = client.db("style-fabrics").collection("products");
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
+        app.get('/product', async(req, res)=>{
+            const query = {}
+            const cursor =  productCollection.find(query);
+            const products= await cursor.toArray()
+            res.send(products); 
+        })
+    }
+    finally{}
+    
+}
+run().catch(console.dir())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!, Style Fabrics Web App')
+  res.send('Hello World!, Welcome to style fabrics app...')
 })
 
 app.listen(port, () => {
