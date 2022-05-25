ff6fc9a15f6c68215935de40b5f241d7d258eb42
Ashraf Ali <ashrafalicitp@gmail.com>
2022-05-25 16:50:21 +0600

    verfiy token create

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port =  process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -12,11 +13,28 @@ app.use(express.json())
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.asxnk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req,res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'UnAuthorization access'});
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+    if(err){
+      return res.status(403).send({message: 'Forbidden access'})
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
     try{
         await client.connect();
         const productCollection = client.db("style-fabrics").collection("products");
         const orderCollection = client.db("style-fabrics").collection("orders");
+        const userCollection = client.db("style-fabrics").collection("users");
 
         app.get('/product', async(req, res)=>{
             const query = {}
@@ -25,7 +43,29 @@ async function run() {
             res.send(products); 
         });
 
- /*
+        /*
+        * user api
+        */  
+
+        app.get('/user', async(req,res)=>{
+          const users = await userCollection.find().toArray();
+          res.send(users);
+        });
+
+       app.put('/user/:email', async(req,res)=>{
+         const email =req.params.email;
+         const user = req.body;
+         const filter = {email: email};
+         const options ={upsert: true};
+         const updateDoc = {
+           $set: user
+         };
+         const result = await userCollection.updateOne(filter, updateDoc, options);  
+         const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECETE, { expiresIn: '1h' });
+         res.send({result, token});
+       })
+
+        /*
         * available api
         */  
         app.get('/available', async (req, res) => {
@@ -49,17 +89,18 @@ async function run() {
          /*
         * order api
         */    
-        app.get('/order', async(req,res)=>{
+        app.get('/order',verifyJWT, async(req,res)=>{
           const user= req.query.user; 
-          const query = {user:user}
-          const cursor =  orderCollection.find(query);
-          const orders= await cursor.toArray()
-          res.send(orders); 
-
-          // const user= req.query.user; 
-          // const query ={user:user};
-          // const orders = await orderCollection.find(query).toArray();
-          // res.send(orders);
+          const decodedEmail = req.decoded.email;
+          if(patient === decodedEmail){
+            const query = {user:user}
+            const cursor =  orderCollection.find(query);
+            const orders= await cursor.toArray()
+            return res.send(orders); 
+          }
+          else{
+            return res.status(403).send({message: 'Forbidden access'})
+          }
         })
 
        app.post('/order', async(req,res)=>{
