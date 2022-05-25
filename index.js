04120217c893c4a9e1745a769132bcab8b5496f7
Ashraf Ali <ashrafalicitp@gmail.com>
2022-05-26 03:17:20 +0600

    admin role veryfity

--- index.js
+++ index.js
@@ -65,7 +65,12 @@ async function run() {
           res.send(users);
         });
 
-       
+        app.get('/admin/:email', async(req, res) =>{
+          const email = req.params.email;
+          const user = await userCollection.findOne({email: email});
+          const isAdmin = user.role === 'admin';
+          res.send({admin: isAdmin})
+        });
 
 
         app.put('/user/admin/:email',verifyJWT, async(req,res)=>{
